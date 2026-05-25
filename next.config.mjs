8716163cf7c9bbfc11776651950af8114adfe659
SATYAM SINGH <satyam6688singh@gmail.com>
2026-05-26 05:19:29 +0530

    Update website

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["images.unsplash.com", "img.clerk.com"],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+    ],
   },
 };
 
